2878b51cb239385e4a4104c42a82383be3869ada
youssef walid mahamed adel <73849250+gousefw0@users.noreply.github.com>
2023-06-21 02:52:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def helper_func(stock,a):
     predictions = []
     # walk-forward validation
     import statsmodels.api as sm
-    model = ARIMA( data['Close'], order=((3,2,3)))
+    model = ARIMA( data['Close'], order=((2,2,1)))
     model_fit = model.fit()
     output = model_fit.forecast(a)
     res=[]
